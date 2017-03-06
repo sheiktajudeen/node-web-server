ead52db833c5497cbadd87ede1c56ccdf87b229b
stajud001c <sheik_tajudeen@comcast.com>
2017-03-05 23:14:03 -0700

    Added a new project page

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Portfolio Page',
+    welcomeMessage: 'Welcome to Portfolio Page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to process request'
